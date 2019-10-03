d61df14cc6c8a77c4ee13a47b885aa5019112707
David Kajpust <kajpustd@gmail.com>
2019-10-02 22:57:42 -0400

    Add handleReveal, handleSubmit, and handleCommit

--- src/mappings/TCR.ts
+++ src/mappings/TCR.ts
@@ -1,15 +1,14 @@
 /* eslint-disable prefer-const */ // to satisfy AS compiler
 
-import {BigInt,Address} from '@graphprotocol/graph-ts'
+import { BigInt, ipfs, json, Bytes } from '@graphprotocol/graph-ts'
 import {
   Project,
   Submission,
   Listing,
   Metadata,
-  Poll,
   Token,
   Curator,
-  TCR
+  TCR,
 } from '../types/schema'
 import {
   Deployed,
@@ -21,12 +20,17 @@ import {
   Commit,
   Reveal,
   Processed,
-  Claim
+  Claim,
+  TCR as tcrContract,
 } from '../types/TCR/TCR'
 
+// TODO - the interfaces for app type
+// TODO - handle general transfer of the token through token transfer events
+  // or just always call the contract for the curator balance
 
+// Deploy the TCR
 export function handleDeployed(event: Deployed): void {
-  let tcr = new TCR("1")
+  let tcr = new TCR('1')
   tcr.owner = event.params.owner
   tcr.votingDurationSecs = event.params.votingDurationSecs
   tcr.revealDurationSecs = event.params.revealDurationSecs
@@ -34,27 +38,29 @@ export function handleDeployed(event: Deployed): void {
   tcr.symbol = event.params.symbol
   tcr.decimals = event.params.decimals
   tcr.bootstrapListAddress = event.params.bootstrapList
-  tcr.started = false
+  tcr.ready = false
+  tcr.memberIndex = BigInt.fromI32(0)
+  tcr.currentBallotIndex = BigInt.fromI32(0)
   tcr.save()
 }
 
-
+// Start the TCR
 export function handleStarted(event: Started): void {
-  let tcr = new TCR("1")
-  tcr.started = true
+  let tcr = new TCR('1')
+  tcr.startDate = event.block.timestamp
   tcr.save()
-
 }
 
-// Might delete this
+// TODO Might delete this
 export function handleBootstrap(event: Bootstrap): void {
 
 }
 
+// Buy TCR Tokens
 export function handleBuy(event: Buy): void {
   let id = event.params.buyer.toHexString()
   let curator = Curator.load(id)
-  if (curator == null){
+  if (curator == null) {
     curator = new Curator(id)
     curator.votes = []
     curator.tokenBalance = BigInt.fromI32(0)
@@ -63,10 +69,11 @@ export function handleBuy(event: Buy): void {
   curator.save()
 }
 
+// Sell TCR tokens
 export function handleSell(event: Sell): void {
   let id = event.params.seller.toHexString()
   let curator = Curator.load(id)
-  if (curator == null){
+  if (curator == null) {
     curator = new Curator(id)
     curator.votes = []
     curator.tokenBalance = BigInt.fromI32(0)
@@ -75,38 +82,117 @@ export function handleSell(event: Sell): void {
   curator.save()
 }
 
-// TODO - handle general transfer of the token through token transfer events
-// or just always call the contract for the curator balance
-
+// Creating a listing
 export function handleSubmit(event: Submit): void {
+  let id = event.params.pollId.toHexString()
+  let listing = new Listing(id)
+  listing.action = event.params.action
+  listing.applicant = event.params.owner.toHexString()
+  listing.metadataHash = event.params.details
+  listing.entry = event.params.entry
+  listing.amount = event.params.amount
+  listing.status = 'Voting'
 
-  // creates a listing (basically a ballot)
-  // creates a poll
-  // creates a tally, but we dont have that in schema yet....
-    // maybe kind of like a vote?
+  // Need to call the contract here since poll data isn't emitted in event
+  let contract = tcrContract.bind(event.address)
+  let pollData = contract.pollQueue(event.params.pollId)
+  listing.startTime = pollData.value0
+  listing.endTime = pollData.value1
+  listing.revealTime = pollData.value2
 
+  // Need to call the contract here since tally data isn't emitted in event
+  let tallyData = contract.tallyQueue(event.params.pollId)
+  listing.yesVotes = tallyData.value0
+  listing.noVotes = tallyData.value1
+  listing.unrevealedAmountTotal = tallyData.value2
+  listing.save()
+
+  let tcr = new TCR('1')
+  tcr.currentBallotIndex = event.params.currentPollId
+  tcr.save()
+
+
+
+  // Take metadataHash and query IPFS and create Metadata
+  let metadata = new Metadata(event.params.details)
+  let ipfsData = ipfs.cat(event.params.details)
+  if (ipfsData !== null) {
+    let data = json.fromBytes(ipfsData as Bytes).toObject()
+    if (data.get('name')) {
+      metadata.name = data.get('name').toString()
+    }
+    if (data.get('description')) {
+      metadata.description = data.get('description').toString()
+    }
+    if (data.get('logo')) {
+      metadata.logo = data.get('logo').toString()
+    }
+    if (data.get('category')) {
+      metadata.category = data.get('category').toString()
+    }
+    if (data.get('website')) {
+      metadata.website = data.get('website').toString()
+    }
+    if (data.get('blog')) {
+      metadata.blog = data.get('blog').toString()
+    }
+    if (data.get('socialFeed')) {
+      metadata.socialFeed = data.get('socialFeed').toString()
+    }
+    if (data.get('sourceCode')) {
+      metadata.sourceCode = data.get('sourceCode').toString()
+    }
+    metadata.save()
+  }
 }
 
+// Voting on a listing
 export function handleCommit(event: Commit): void {
+  let id = event.params.pollId.toHexString().concat('-').concat(event.params.voter.toHexString())
+  let submission = new Submission(id)
+  submission.listing = event.params.pollId.toHexString()
+  submission.amount = event.params.amount
+  submission.claimed = false
+  submission.save()
 
-  // you commit tokens to a ballot/listing (dont vote yet)
-
+  let listing = Listing.load(event.params.pollId.toHexString())
+  listing.unrevealedAmountTotal = listing.unrevealedAmountTotal.plus(event.params.amount)
+  listing.save()
 }
 
+// Revealing your vote on a listing
 export function handleReveal(event: Reveal): void {
+  let id = event.params.pollId.toHexString().concat('-').concat(event.params.voter.toHexString())
+  let submission = new Submission(id)
+  submission.vote = event.params.vote
+  submission.save()
 
-  // this is where you vote
-
+  let listing = Listing.load(event.params.pollId.toHexString())
+  listing.unrevealedAmountTotal = listing.unrevealedAmountTotal.minus(event.params.amount)
+  if (event.params.vote == 1) {
+    listing.yesVotes = listing.yesVotes.plus(event.params.amount)
+  } else {
+    listing.noVotes = listing.noVotes.plus(event.params.amount)
+  }
+  listing.save()
 }
 
+// Finalizing a listing, and creating a Project
 export function handleProcessed(event: Processed): void {
+  let listing = Listing.load(event.params.pollId.toHexString())
+  listing.pr
 
-  // decide if it wins or loses
+  let tcr = new TCR('1')
+  tcr.currentBallotIndex = event.params.pollId
+
+  let nextListing = Listing.load(event.params.pollId.toHexString())
+  // todo - continue here
 
 }
 
+// Claiming back tokens if you voted correctly
 export function handleClaim(event: Claim): void {
 
-  // claim back your tokens if you won
+
 
 }
